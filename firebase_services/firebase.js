c34a9da62822e318993b98df239c000fda1e1a57
JayBuck403 <jaybuck403@gmail.com>
2025-07-09 02:24:41 +0000

    Added and tested all endpoints

--- firebase_services/firebase.js
+++ firebase_services/firebase.js
@@ -3,7 +3,7 @@ var admin = require("firebase-admin");
 // var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(require('./secrets/firebase-service-account.json')),
+  credential: admin.credential.cert(require('../secrets/dwello-homes-firebase-adminsdk-fbsvc-166b167823.json')),
 });
 
 module.exports = admin;
