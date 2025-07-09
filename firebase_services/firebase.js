1f5d0858892b14944a27bf3e026df00e974ae111
JayBuck403 <jaybuck403@gmail.com>
2025-07-09 02:20:57 +0000

    Added and tested all endpoints

--- firebase_services/firebase.js
+++ firebase_services/firebase.js
@@ -1,9 +1,9 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(require('./secrets/firebase-service-account.json')),
 });
 
 module.exports = admin;
