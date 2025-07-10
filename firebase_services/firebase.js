2cb0e2ac4130a7ea0bcff475db4b9cf186664f7e
JayBuck403 <jaybuck403@gmail.com>
2025-07-10 18:28:03 +0000

    Added necessary files to env

--- firebase_services/firebase.js
+++ firebase_services/firebase.js
@@ -1,9 +1,11 @@
 var admin = require("firebase-admin");
 
-// var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+var serviceAccount = JSON.parse(
+  require(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(require('../secrets/dwello-homes-firebase-adminsdk-fbsvc-166b167823')),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
